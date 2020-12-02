6d7fef0905d41b095a5333f8fb0b2c64c1597cd2
pakawat2549 <75378790+pakawat2549@users.noreply.github.com>
2020-12-02 22:01:56 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 13) {
+        while (maqueen.Ultrasonic(PingUnit.Centimeters) > 15) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        }
+    }
+})
